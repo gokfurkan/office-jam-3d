00c5fa9f3f74b7d34e10549027a043c5881a6bee
Furkan Gök <furkangok270@gmail.com>
2024-02-17 17:36:31 +0300

    update chiar , desk models , add floor piece changer for level design

---
+++ Assets/Game/Dev/Scripts/FloorPieceChanger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Game.Dev.Scripts
+{
+    public class FloorPieceChanger : MonoBehaviour
+    {
+        [SerializeField] private MeshRenderer meshRenderer;
+        [SerializeField] private List<Material> floorMaterials;
+        
+        private int currentMaterialIndex = 0;
+        
+        [Button]
+        public void ChangeMaterial()
+        {
+            currentMaterialIndex = (currentMaterialIndex + 1) % floorMaterials.Count;
+            meshRenderer.material = floorMaterials[currentMaterialIndex];
+        }
+    }
+}
